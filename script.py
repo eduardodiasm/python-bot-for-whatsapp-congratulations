c3f79860d23e8b6b73b3c3ab33d17315a063a390
Eduardo Dias <eduardodiasprog@gmail.com>
2020-01-28 00:36:45 -0300

    looking for the messages

--- script.py
+++ script.py
@@ -6,3 +6,12 @@ driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
 driver.get('https://web.whatsapp.com/')
 
 input('press any key after QR scan ')
+time.sleep(4)
+
+people_names = ['personName']
+
+for person_name in people_names:
+    person = driver.find_element_by_xpath('//span[@title = "{}"]'.format(person_name))
+    person.click()
+    for i in range(1, 3):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
